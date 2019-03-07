6948a17acd2eed72ef92367d57d04d7743f39647
jleblond <jasmine.leblondc@gmail.com>
2019-03-07 10:40:58 -0500

    task edit assignations updates correctly, admin allowed to edit-update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :require_author_permission, only: [:edit, :update]
+  before_filter :require_edit_permission, only: [:edit, :update]
 
   def index
     @tasks_assigned = current_user.tasks_assigned
@@ -72,15 +72,22 @@ class TasksController < ApplicationController
 
 
     if @task.update_attributes(params[:task])
-      params[:users_assigned].each { |u_id|
-        if !u_id.blank? && Assignation.where(task_id: @task.id, user_id: u_id).empty?
-         Assignation.create(task_id: @task.id, user_id: u_id)
-        end
+
+      # Delete assignations for which their user_id does not match a user_id in params[:users_assigned]
+      assignations = Assignation.where(task_id:@task.id)
+      assignations.pluck(:user_id).each { |a|
+          unless params[:users_assigned].include?(a.to_s)
+           assignation_id = Assignation.where(task_id: @task.id, user_id: a).pluck(:id)
+           Assignation.destroy(assignation_id)
+          end
       }
 
-
-
-
+      # Create assignations that were added to the form
+      params[:users_assigned].each { |u_id|
+        if !u_id.blank? && Assignation.where(task_id: @task.id, user_id: u_id).empty?
+        Assignation.create(task_id: @task.id, user_id: u_id)
+        end
+      }
       flash[:success] = "Task updated"
       redirect_to(task_path(@task))
     else
@@ -94,8 +101,8 @@ class TasksController < ApplicationController
 
   private
 
-  def require_author_permission
-    if current_user != Task.find(params[:id]).author
+  def require_edit_permission
+    if current_user != Task.find(params[:id]).author && !current_user.admin?
       redirect_to(tasks_path(filter:"authored"))
     end
   end
