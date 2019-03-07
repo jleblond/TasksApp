b09feb858d433b12e9b24c23c04e2a92c658b53b
jleblond <jasmine.leblondc@gmail.com>
2019-03-07 17:36:56 -0500

    css more advanced

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,9 +4,9 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
 
-    @notifications.pluck(:id).each do |n_id|
-      Notification.find(n_id).update_attributes(read: true)
-    end
+    # @notifications.pluck(:id).each do |n_id|
+    #   Notification.find(n_id).update_attributes(read: true)
+    # end
 
   end
 
